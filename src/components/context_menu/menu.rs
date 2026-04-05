73a5282a579f5432432c7051f0247a464dcaf730
Esa Särkelä <esas4rkela@gmail.com>
2026-04-05 19:42:58 +0300

    fix context menu overflowing right side of screen

--- src/components/context_menu/menu.rs
+++ src/components/context_menu/menu.rs
@@ -7,9 +7,25 @@ pub struct Props {
     pub children: Children
 }
 
+const MENU_WIDTH: f64 = 100.0; // ~approximate
+
 #[function_component(ContextMenu)]
 pub fn context_menu(props: &Props) -> Html {
-    let position = format!("position: absolute; left: {}px; top: {}px;", props.pos.x, props.pos.y);
+    let window_width = web_sys::window()
+        .unwrap()
+        .inner_width()
+        .unwrap()
+        .as_f64()
+        .unwrap();
+
+    // adjust context-menu position if too close to the right side to prevent it from overflowing
+    let adjusted_x = if props.pos.x + MENU_WIDTH > window_width {
+        props.pos.x - MENU_WIDTH
+    } else {
+        props.pos.x
+    };
+
+    let position = format!("position: absolute; left: {}px; top: {}px;", adjusted_x, props.pos.y);
 
     html! {
         <menu class="context-menu" style={position}>
